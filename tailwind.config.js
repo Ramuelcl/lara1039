0305a556b1f2c1748f6b5ff025c9b204436554fc
Ramuelcl <ramuelcl@gmail.com>
2023-12-31 12:24:24 +0100

    main - tall

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config}
+export default {
+    darkMode: 'class',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+*/
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
