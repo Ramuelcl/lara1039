a16855e65d9994bd2ff62d65942d85b2d670b959
Ramuelcl <ramuelcl@gmail.com>
2023-12-31 18:47:48 +0100

    main - breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
-/** @type {import('tailwindcss').Config}
-export default {
-    darkMode: 'class',
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-*/
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: 'class',
+export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [],
-}
+
+    plugins: [forms],
+};
